5bb0b25c05f33c27ddad106122b58cd5dd250c5d
Cyberia 2045 <sagansociety86@gmail.com>
2017-03-15 15:51:35 -0400

    fixed animation transitions

--- schoold/app/assets/javascripts/waypoints.js
+++ schoold/app/assets/javascripts/waypoints.js
@@ -1,16 +1,33 @@
 $(document).ready (function() {
 
 	$('.secondary_background').waypoint(function(direction) {
-		$('.main-content').css("animation", "fadeIn 3s")
-		$(".main-content").css("opacity", 1)
-		$('.header2').css("animation", "fadeIn 3s")
-		$('.header2').css("opacity", 1)
+		$('.main-content').css("animation-name", "something")
+		$(".main-content").css("animation-duration", "5s")
+		$(".main-content").css("opacity", "1")
+		$('.header2').css("animation-name", "something")
+		$('.header2').css("animation-duration", "5s")
+		$(".header2").css("opacity", "1")
 		}, 
 		{ offset: "40%"});
 
-	// $('.secondary_background').waypoint(function() {
+	$('.tertiary_background').waypoint(function(direction) {
+		$('#dom-container').css("animation-name", "something")
+		$("#dom-container").css("animation-duration", "5s")
+		$("#dom-container").css("opacity", "1")
+		}, 
+		{ offset: "40%"});
 
-	// },
-	// { offset: "40%" });
+	$('.tertiary_background').waypoint(function(direction) {
+		$('#john-container').css("animation-name", "something")
+		$("#john-container").css("animation-duration", "5s")
+		$("#john-container").css("opacity", "1")
+		}, 
+		{ offset: "20%"});
+
+	$('.tertiary_background').waypoint(function(direction) {
+		$('#mateo-container').css("animation-name", "something")
+		$("#mateo-container").css("animation-duration", "5s")
+		$("#mateo-container").css("opacity", "1")
+	});
 
 });//End of document.ready
