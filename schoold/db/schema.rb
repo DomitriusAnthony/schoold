ede54a91092eb8a15963bde51fccd709e2ffea31
Cyberia 2045 <sagansociety86@gmail.com>
2017-03-17 16:57:13 -0400

    changed image sourcing

--- schoold/db/schema.rb
+++ schoold/db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170316210750) do
 
   create_table "challenges", force: :cascade do |t|
@@ -25,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170316210750) do
     t.string   "video_tag"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -45,6 +45,6 @@ ActiveRecord::Schema.define(version: 20170316210750) do
     t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  
+  end
 
 end
