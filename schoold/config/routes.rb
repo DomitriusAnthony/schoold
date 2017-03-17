5c2cf5ac93daa832e805ecfb6b3052fa98566a3f
Cyberia 2045 <sagansociety86@gmail.com>
2017-03-17 16:13:59 -0400

    fixed routing

--- schoold/config/routes.rb
+++ schoold/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: "home#index"
 
 get "profile" => "home#profile", as: "profile"
+
+get "/users/:id" => "users#profile", as: "user_profile"
 
 end
