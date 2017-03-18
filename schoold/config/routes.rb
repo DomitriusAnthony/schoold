c175faa859956d3caac0c5a504fbc4635056228f
Cyberia 2045 <sagansociety86@gmail.com>
2017-03-18 00:11:55 -0400

    started CSS challenges

--- schoold/config/routes.rb
+++ schoold/config/routes.rb
@@ -13,4 +13,10 @@ get "/challenge_two", to: "challenges#heading"
 get "/challenge_three", to: "challenges#about"
 get "/challenge_four", to: "challenges#gallery"
 get "/challenge_five", to: "challenges#video"
+
+get "/css_challenge_1", to: "challenges#picture_positioning"
+get	"/css_challenge_2", to: "challenges#layout"
+get	"/css_challenge_3", to: "challenges#background"
+get	"/css_challenge_4", to: "challenges#typography"
+get	"/css_challenge_5", to: "challenges#styling"
 end
