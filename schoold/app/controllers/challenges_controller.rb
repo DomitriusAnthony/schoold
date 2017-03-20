88594d160e6a59dcbe28b10c9af730b7afaa92a7
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-03-20 13:53:09 -0400

    linked challenge pages

--- schoold/app/controllers/challenges_controller.rb
+++ schoold/app/controllers/challenges_controller.rb
@@ -1,13 +1,13 @@
 class ChallengesController < ApplicationController
-	def profile_picture
-		@challenge = Challenge.new
-		@user = User.find(current_user)
-	 end
+	before_action :set_challenge, only: [:update_heading, :update_paragraph, :update_image_one, :update_image_two, :update_image_three, :update_video_tag]
 
-	 def update_profile
-	 	@challenge = challenge.update(profile_pic: params[:profile_pic])
 
-	 	 if @challenge.update
+
+	def update_profile
+		@challenge = Challenge.create(profile_pic: "")
+		@challenge = challenge.update(profile_pic: params[:profile_pic])
+
+	 	 if @challenge.save
 	 	 	format.html { redirect_to "/challenge_two" }
 	 	 end
 	 end
@@ -40,9 +40,15 @@ class ChallengesController < ApplicationController
 	 	@challenge = challenge.update(video_tag: params[:video_tag])
 	 end
 
-	 # private 
+
+
+	private
+
+	def set_challenge
+		@challenge = Challenge.find(1)
+	end 
 	 
-	 # def challenge_params
-	 # 	params.require(:challenge).permit(:profile_pic, :heading_tag, :p_tag, :image_one, :image_two, :image_three, :video_tag)
-	 # end
+	 def challenge_params
+	 	params.require(:challenge).permit(:profile_pic, :heading_tag, :p_tag, :image_one, :image_two, :image_three, :video_tag)
+	 end
 end
--- schoold/app/models/user.rb
+++ schoold/app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :challenges
+ 
 end
