cf8c3c5a3eaef842226f82006583fb55a13abfae
Cyberia 2045 <sagansociety86@gmail.com>
2017-03-20 00:24:27 -0400

    fixing merge conflicts

--- schoold/config/routes.rb
+++ schoold/config/routes.rb
@@ -22,4 +22,10 @@ post "/challenge_four", to: "challenges#gallery"
 
 get "/challenge_five", to: "challenges#video"
 post "/challenge_five", to: "challenges#video"
+
+get "/css_challenge_1", to: "challenges#background"
+get	"/css_challenge_2", to: "challenges#picture_positioning"
+get	"/css_challenge_3", to: "challenges#layout"
+get	"/css_challenge_4", to: "challenges#typography"
+get	"/css_challenge_5", to: "challenges#styling"
 end
