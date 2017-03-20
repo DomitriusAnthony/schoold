dbbe99a52ad823628ecf95ef98b32547a8339cb7
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-03-19 22:39:19 -0400

    Created submit and test buttons for challenge forms.

--- schoold/config/routes.rb
+++ schoold/config/routes.rb
@@ -9,8 +9,17 @@ get "/users/:id" => "users#profile", as: "user_profile"
 
 
 get "/challenge_one", to: "challenges#profile_picture"
+post "/challenge_one", to: "challenges#profile_picture"
+
 get "/challenge_two", to: "challenges#heading"
+post "/challenge_two", to: "challenges#heading"
+
 get "/challenge_three", to: "challenges#about"
+post "/challenge_three", to: "challenges#about"
+
 get "/challenge_four", to: "challenges#gallery"
+post "/challenge_four", to: "challenges#gallery"
+
 get "/challenge_five", to: "challenges#video"
+post "/challenge_five", to: "challenges#video"
 end
