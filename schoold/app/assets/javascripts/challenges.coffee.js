319c2bf76d076b55875063fe3794416fb988780c
Cyberia 2045 <sagansociety86@gmail.com>
2017-03-20 00:20:43 -0400

    fixing merge conflicts

--- schoold/app/assets/javascripts/challenges.coffee.js
+++ schoold/app/assets/javascripts/challenges.coffee.js
@@ -62,4 +62,38 @@ $("document").ready(function() {
     
 	$(".video").src = answer
   	})
-})
+
+// ----------CSS Challenges---------
+
+// ----------Challenge One----------
+	$(".code_generator").click(function() {
+	    answer = $("#answer").val()
+	    console.log(answer)
+	    $(".profile_picture").css("margin", answer)
+	  })
+
+// --------Challenge Two------------
+	$(".code_generator").click(function() {
+	    answer = $("#answer").val()
+	    console.log(answer)
+	    $(".custom_background").css("background-color", answer)
+	  })
+
+	// -----Challenge Three----------
+		$(".code_generator").click(function() {
+	    answer = $("#answer_1").val()
+	    console.log(answer)
+	    $(".main_content").css("margin", answer)
+	  })
+			$(".code_generator").click(function() {
+	    answer = $("#answer_2").val()
+	    console.log(answer)
+	    $(".main_content").css("padding", answer)
+	  })
+			$(".code_generator").click(function() {
+	    answer = $("#answer_3").val()
+	    console.log(answer)
+	    $(".main_content").css("text-align", answer)
+	  })
+
+}) // end of Document.ready
